f66a99a5ed0f1ba02020572199ee3071c9da5f46
Ethan Bowen <ethan.a.bowen@gmail.com>
2019-11-26 00:27:37 -0600

    Updates

---
+++ precache-manifest.76f23eb5f8d04e26e6d137b31bcd1d2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7423074815b0084c53dd00ec0cf0b15",
+    "url": "/React-Natours/index.html"
+  },
+  {
+    "revision": "1937982410c7e03c3855",
+    "url": "/React-Natours/static/css/main.ac16af47.chunk.css"
+  },
+  {
+    "revision": "f0e07ab58589f78e6608",
+    "url": "/React-Natours/static/js/2.714a18d5.chunk.js"
+  },
+  {
+    "revision": "1937982410c7e03c3855",
+    "url": "/React-Natours/static/js/main.a8a77b20.chunk.js"
+  },
+  {
+    "revision": "a117cb6c7cad9b6ac036",
+    "url": "/React-Natours/static/js/runtime-main.ff49509e.js"
+  },
+  {
+    "revision": "97a36163858cc16f92b8ed4565ceeab5",
+    "url": "/React-Natours/static/media/hero-2.97a36163.jpg"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/React-Natours/static/media/logo-white.40d4ef82.png"
+  }
+]);
